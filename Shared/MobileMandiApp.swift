49d615ededfe0af6aa2d64b3d522f94a3e9053e8
JaskiratChahal <jaskiratsingh@outlook.com>
2022-05-01 17:58:03 +0100

    Initial Commit

---
+++ Shared/MobileMandiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileMandiApp.swift
+//  Shared
+//
+//  Created by Jaskirat Chahal on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileMandiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
